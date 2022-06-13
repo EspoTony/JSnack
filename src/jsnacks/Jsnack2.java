3f5d1fb2785aa9362ddf9d52abf6c286afdbe627
EspoTony <espotony98@gmail.com>
2022-06-13 11:15:14 +0200

    secondo esercizio fatto

---
+++ src/jsnacks/Jsnack2.java
@@ -0,0 +1,25 @@
+package jsnacks;
+
+//Chiedi all'utente di inserire un numero
+//se è pari stampa
+//se è dispari stampa il successivo
+
+import java.util.Scanner;
+
+public class Jsnack2 {
+	public static void main (String [] args) {
+		
+		Scanner numero = new Scanner(System.in);
+		
+		System.out.println("Inserisci un numero:");
+		int valore = numero.nextInt();
+		
+		numero.close();
+		
+		if (valore%2==0) {
+			System.out.println(valore);
+		} else System.out.println(valore+1);
+		
+		
+	}
+}
